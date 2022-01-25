33c37c2b640c43640467cff1d72f0423b897f250
coder1111111 <97535381+coder1111111@users.noreply.github.com>
2022-01-25 10:19:11 -0500

    Add files via upload

---
+++ project 99/uploader.py
@@ -0,0 +1,27 @@
+import dropbox
+filename = input("input file you want to backup here:")
+fielname2 = input("put just name of file here")
+class TransferData:
+    def __init__(self, access_token):
+        self.access_token = access_token
+
+    def upload_file(self, file_from, file_to):
+        """upload a file to Dropbox using API v2
+        """
+        dbx = dropbox.Dropbox(self.access_token)
+
+        with open(file_from, 'rb') as f:
+            dbx.files_upload(f.read(), file_to)
+
+def main():
+    access_token = 'insert access token from dropbox'
+    transferData = TransferData(access_token)
+
+    file_from = filename
+    file_to = '/test_dropbox/'+fielname2  # The full path to upload the file to, including the file name
+
+    # API v2
+    transferData.upload_file(file_from, file_to)
+
+if __name__ == '__main__':
+    main()
